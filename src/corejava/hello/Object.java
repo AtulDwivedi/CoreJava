1e22d06b5f826585fa8fcbb2f942c2d33c1c11cc
Atul Dwivedi <er.dwivediatul@gmail.com>
2014-05-24 14:28:24 +0530

    Temp

---
+++ src/corejava/hello/Object.java
@@ -0,0 +1,7 @@
+package corejava.hello;
+
+public class Object {
+		public void myObjClassMethod(){
+		System.out.println("My Object class method.");
+	}
+}
---
+++ src/corejava/hello/ObjectTest.java
@@ -0,0 +1,12 @@
+package corejava.hello;
+
+public class ObjectTest extends Object{
+	
+	public static void main(String[] args) {
+		ObjectTest obj = new ObjectTest();
+		
+		obj.myObjClassMethod(); 
+		
+	}
+
+}
