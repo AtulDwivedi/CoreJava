d4007fff6c97a49d478c95da5ccda05d5d6b4efc
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-04-09 21:56:24 +0530

    Added method to UsingClassPreambleAnnotation

--- src/corejava/annotation/UsingClassPreambleAnnotation.java
+++ src/corejava/annotation/UsingClassPreambleAnnotation.java
@@ -6,7 +6,20 @@ package corejava.annotation;
 		version = 1.1f,
 		lastModified = "Atul",
 		by = "Atul D", 
-		reviewers = { "Martin" })
+		reviewers = { "Martin", "Fowler" })
 public class UsingClassPreambleAnnotation {
 
+	public String name = "Sakti";
+	
+	public void myMethod1(){
+		System.out.println("a dkja");
+	}
+	
+	private int myMethod2(){
+		return 0;
+	}
+	
+	protected String myMethod3(){
+		return "";
+	}
 }
